9757f48c020af5905460ebfe62f26fad6dec551c
Nguyễn Nghiệp <nghiep2k3>
2025-08-19 00:15:28 +0700

    code

--- be/public/index.php
+++ be/public/index.php
@@ -1,9 +1,16 @@
 <?php
+
+use App\Core\db;
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__).'/config');
+require __DIR__ . '/../config/db.php';
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../config');
 $dotenv->safeLoad();
 
 // Gọi DB như cũ
+echo "Đang khởi tạo kết nối cơ sở dữ liệu...<br>";
 $db = new db();
 $conn = $db->connect();
